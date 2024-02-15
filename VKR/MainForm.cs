74c42052e4afbbff1b33804a9facc58054c50ccb
@Zefyr17 <wisemole51@gmail.com>
2024-02-15 19:54:28 +0500

    Добавьте файлы проекта.

---
+++ VKR/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VKR;
+
+namespace VKR
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            AuthForm authForm = new AuthForm();
+            authForm.ShowDialog();
+
+            if (!AuthClass.IsAuthenticated)
+            {
+                Application.Exit();
+            }
+
+            toolStripLabel1.Text = $"Добро пожаловать {AuthClass.auth_fio}! Ваш код {AuthClass.auth_id.ToString()}";
+
+            switch (AuthClass.auth_role)
+            {
+                case 3:
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    break;
+                case 2:
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    break;
+                case 1:
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                    break;
+                default:
+                    MessageBox.Show("Ваша роль в системе не определена. Приложение будет закрыто!");
+                    Application.Exit();
+                    break;
+            }
+
+        }
+    
+    }
+}
---
+++ VKR/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VKR
+{
+    static class AuthClass
+    {
+        // Static field that stores the authentication status
+        public static bool IsAuthenticated = false;
+        // Static field that stores the user ID
+        public static int auth_id = 0;
+        // Static field that stores the user's full name
+        public static string auth_fio = null;
+        // Static field that stores the user's role title
+        public static string auth_role_title = null;
+        // Static field that stores the user's role privileges
+        public static int auth_role = 0;
+
+        static string sha256(string randomString)
+        {
+            // Cryptographic magic happens here. The purpose of this method is to hash the input string
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            Application.Run(new MainForm());
+        }
+    }
+}
+
